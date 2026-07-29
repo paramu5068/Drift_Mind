ac3b148c06412baa23dfc31e5d25dbd64c4e054c
paramu5068 <rparameswari2005@gmail.com>
2026-07-29 14:18:46 +0530

    Fix web blank screen error by initializing Firebase with DefaultFirebaseOptions

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'core/theme/app_theme.dart';
 import 'features/splash/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    debugPrint('Firebase initialization warning: $e');
+  }
+
   await Hive.initFlutter();
   await Hive.openBox('settings');
   
